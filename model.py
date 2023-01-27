ce4b5b4aa0923530b8ad6fe759da45da6f42741d
Achal Kumar Garg <118976187+achalgarg14@users.noreply.github.com>
2023-01-27 13:09:09 +0530

    Adding model.py file

---
+++ model.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, nm='BN', gp = 2):
+        super(Net, self).__init__()
+
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            self.normalization(nm,8,gp)        
+        ) # output_size = 26 RF = 3
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            self.normalization(nm,16,gp)
+        ) # output_size = 24 RF = 5
+
+        # TRANSITION BLOCK 1        
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(1, 1), padding=0, bias=False)
+        ) # output_size = 24 RF = 5
+
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12 RF = 6
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            self.normalization(nm,16,gp)
+        ) # output_size = 10 RF = 10
+
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            self.normalization(nm,16,gp)
+        ) # output_size = 8 RF = 14
+
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=20, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            self.normalization(nm,20,gp)
+        ) # output_size = 8 RF = 18
+
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=8)
+        ) # output_size = 1 RF = 28
+
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)
+        )
+        self.dropout = nn.Dropout(0.05)
+
+    def forward(self, x):
+        x = self.dropout(self.convblock1(x))
+        x = self.dropout(self.convblock2(x))
+        x = self.pool1(self.convblock3(x))
+        x = self.dropout(self.convblock4(x))
+        x = self.dropout(self.convblock5(x))
+        x = self.dropout(self.convblock6(x))
+        x = self.gap(x)
+        x = self.convblock7(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
+
+    def normalization(self, nm, channels, group = 2):
+        if nm == 'BN':
+            return nn.BatchNorm2d(channels)
+        elif nm == 'LN':
+            return nn.GroupNorm(1,channels) #equivalent to LayerNorm mentioned at https://pytorch.org/docs/stable/generated/torch.nn.GroupNorm.html
+        elif nm == 'GN':
+            return nn.GroupNorm(group,channels) #group is by default 2
